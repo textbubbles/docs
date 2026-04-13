918f1d06751d1adfa0eef031b6b5f1b606e77976
Lamar Oliphant <loliphant@users.noreply.github.com>
2026-04-12 20:06:36 -0700

    docs: remove untested Chats, Contacts, and Payments features (#4)

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -3,9 +3,6 @@ export default {
   "getting-started": "Getting Started",
   "authentication": "Authentication",
   "messages": "Messages",
-  "chats": "Chats",
-  "contacts": "Contacts",
-  "payments": "Payments",
   "webhooks": "Webhooks",
   "sdk": "SDK",
   "api-reference": "API Reference"
--- pages/payments/_meta.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  "index": "Apple Cash"
-}
